20731f1cf9bbfdec976eaa3e8f7b90fe307508b5
David Suárez <david.suarez.fuentes@gmail.com>
2019-02-07 20:07:00 +0100

    Improve integration tests by alowing to set the number of runs

--- it/test.01.sh
+++ it/test.01.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 display_usage() {
-    echo -e "\nUsage:\ntest.01.sh <hostname:port>"
+    echo -e "\nUsage:\ntest.01.sh -n <number-of-runs> <hostname:port>"
 }
 
-if [  $# -le 0 ] 
+if [  $# -le 1 ] 
 	then 
 		display_usage
 		exit 1
@@ -15,7 +15,9 @@ if [[ ( $# == "--help") ||  $# == "-h" ]]
 		exit 0
 	fi 
 
-time for i in {1..10};
+echo -e "Sending to" http://$3/api/v1.0/documents/convert $2 times
+
+time for i in $(seq 1 $2);
 do
-    find ../examples -name '*' -type f -exec curl -F file=@{} http://$1/api/v1.0/documents/convert \;
+    find ../examples -name '*' -type f -exec curl -F file=@{} http://$3/api/v1.0/documents/convert \;
 done
