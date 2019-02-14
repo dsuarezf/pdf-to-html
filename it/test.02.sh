05beab24cd0b13cf24f581ba4de56cc490842ab3
David Suárez <david.suarez.fuentes@gmail.com>
2019-02-14 20:26:25 +0100

    Add new test using concurrent requests
    Signed-off-by: David Suárez <david.suarez.fuentes@gmail.com>

---
+++ it/test.02.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+display_usage() {
+    echo -e "\nUsage:\ntest.02.sh -f <folder> -n <number-of-processes> <hostname:port>"
+}
+
+if [  $# -le 4 ]
+	then
+		display_usage
+		exit 1
+	fi
+
+if [[ ( $# == "--help") ||  $# == "-h" ]]
+	then
+		display_usage
+		exit 0
+	fi
+
+echo -e "Sending to" http://$5/api/v1.0/documents/convert $4 processes
+
+time (
+  for i in $(seq 1 $4);
+  do
+	find $2 -name '*' -type f -exec curl -F file=@{} http://$5/api/v1.0/documents/convert \; &
+  done
+  wait
+)
