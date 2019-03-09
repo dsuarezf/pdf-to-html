575df61610f91e99d4a99681765183ba360d7dfe
David Suárez <david.suarez.fuentes@gmail.com>
2019-03-09 10:54:15 +0100

    Fix integration tests scripts

--- it/test.01.sh
+++ it/test.01.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+
 display_usage() {
     echo -e "\nUsage:\ntest.01.sh -n <number-of-runs> <hostname:port>"
 }
+
+if [[ ( "$1" == "--help") ||  "$1" == "-h" ]] 
+	then 
+		display_usage
+		exit 0
+	fi 
 
 if [  $# -le 2 ] 
 	then 
 		display_usage
 		exit 1
 	fi
-
-if [[ ( $# == "--help") ||  $# == "-h" ]] 
-	then 
-		display_usage
-		exit 0
-	fi 
 
 echo -e "Sending to" http://$3/api/v1.0/documents/convert $2 times
 
--- it/test.02.sh
+++ it/test.02.sh
@@ -4,16 +4,16 @@ display_usage() {
     echo -e "\nUsage:\ntest.02.sh -f <folder> -n <number-of-processes> <hostname:port>"
 }
 
+if [[ ( "$1" == "--help") ||  "$1" == "-h" ]]
+	then
+		display_usage
+		exit 0
+	fi
+
 if [  $# -le 4 ]
 	then
 		display_usage
 		exit 1
-	fi
-
-if [[ ( $# == "--help") ||  $# == "-h" ]]
-	then
-		display_usage
-		exit 0
 	fi
 
 echo -e "Sending to" http://$5/api/v1.0/documents/convert $4 processes
