11b3ef1ab6a4ec7b9da256b0a515e1f2c50b0905
David Suárez <david.suarez.fuentes@gmail.com>
2019-09-29 21:35:01 +0200

    Improve integration test
    Signed-off-by: David Suárez <david.suarez.fuentes@gmail.com>

--- it/test.01.sh
+++ it/test.01.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 display_usage() {
-    echo -e "\nUsage:\ntest.01.sh -n <number-of-runs> <hostname:port>"
+    echo -e "\nUsage:\ntest.01.sh -f <folder> -n <number-of-runs> <hostname:port>"
 }
 
-if [[ "$1" == "--help" ||  "$1" == "-h" ]] 
-	then 
+if [[ "$1" == "--help" ||  "$1" == "-h" ]]
+	then
 		display_usage
 		exit 0
-	fi 
+	fi
 
-if [  $# -le 2 ] 
-	then 
+if [ $# -le 4 ]
+	then
 		display_usage
 		exit 1
 	fi
 
-echo -e "Sending to" http://$3/api/v1.0/documents/convert $2 times
+echo -e "Sending to" http://$3/api/v1.0/documents/convert $4 times
 
-time for i in $(seq 1 $2);
+time for i in $(seq 1 $4);
 do
-    find ../examples -name '*' -type f -exec curl -F file=@{} http://$3/api/v1.0/documents/convert \;
+    find $2 -name '*' -type f -exec curl -F file=@{} http://$5/api/v1.0/documents/convert \;
 done
