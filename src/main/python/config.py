37b7b1f074c46d2fe90215858fbbbc363da80b5b
David Suárez <david.suarez.fuentes@gmail.com>
2019-09-30 20:21:21 +0200

    Fix Pylint warnings
    Signed-off-by: David Suárez <david.suarez.fuentes@gmail.com>

--- src/main/python/config.py
+++ src/main/python/config.py
@@ -1,14 +1,33 @@
-class Config(object):
+""" Different configurations to be used by the Flask server
+"""
+
+
+class Config:  # pylint: disable=too-few-public-methods
+    """
+    Common configuration
+    """
+
+
+DEBUG = False
+TESTING = False
+
+
+class ProductionConfig(Config):  # pylint: disable=too-few-public-methods
+    """
+    Production configuration
+    """
     DEBUG = False
-    TESTING = False
 
 
-class ProductionConfig(Config):
-    DEBUG = False
-
-class DevelopmentConfig(Config):
+class DevelopmentConfig(Config):  # pylint: disable=too-few-public-methods
+    """
+    Development configuration
+    """
     DEBUG = True
 
 
-class TestingConfig(Config):
+class TestingConfig(Config):  # pylint: disable=too-few-public-methods
+    """
+    Testing configuration
+    """
     TESTING = True
